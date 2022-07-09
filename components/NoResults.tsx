149a17bfb765feaf99e1ed792bd69dad469bbf51
Matthew Fogarty <mattempyre@gmail.com>
2022-07-10 00:19:17 +0200

    Setup api calls and created videocard and noresult

---
+++ components/NoResults.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoResults = (text) => {
+  return <div>NoResults</div>;
+};
+
+export default NoResults;
---
+++ components/VideoCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoCard = (post) => {
+  return <div>VideoCard</div>;
+};
+
+export default VideoCard;
