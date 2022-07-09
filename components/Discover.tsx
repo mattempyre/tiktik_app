afa16eea9d1b532cef0858ca3474034d6d0057ad
Matthew Fogarty <mattempyre@gmail.com>
2022-07-09 22:36:04 +0200

    Fixed bug in _app and Created Sidebar

---
+++ components/Discover.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Discover = () => {
+  return <div>Discover</div>;
+};
+
+export default Discover;
---
+++ components/SuggestAccounts.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SuggestAccounts = () => {
+  return <div>SuggestAccounts</div>;
+};
+
+export default SuggestAccounts;
